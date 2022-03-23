34189adad4f5282f51f4949de7df5a2b4ad036be
Florian Friedrich <ffried@me.com>
2022-03-23 17:52:36 +0100

    Remove no longer needed 'escaped' tag

--- Sources/RouteDocs/Extensions/Application+SetupDocs.swift
+++ Sources/RouteDocs/Extensions/Application+SetupDocs.swift
@@ -22,6 +22,9 @@ extension ViewRenderer {
 }
 
 extension NIOLeafFiles {
+    /// The default leaf source for the docs.
+    /// - Parameter fileio: The non-blocking File IO to use.
+    /// - Returns: The NIO files instance that represents the bundled default docs.
     public static func defaultDocs(with fileio: NonBlockingFileIO) -> Self {
         NIOLeafFiles(fileio: fileio,
                      limits: [.onlyLeafExtensions, .toSandbox, .requireExtensions], // We must not insert .toVisibleFiles or we can't load from the `.build` directory.
@@ -32,16 +35,21 @@ extension NIOLeafFiles {
 }
 
 extension Application {
+    /// The default leaf source for the docs.
     @inlinable
     public var defaultDocsLeafSource: some LeafSource { NIOLeafFiles.defaultDocs(with: fileio) }
 }
 
 extension LeafSources {
+    /// This registers the source "docs" using the given fileio.
+    /// - Parameter fieio: The non-blocking File IO to use.
     @inlinable
     public func addDefaultDocsSource(with fileio: NonBlockingFileIO) throws {
         try register(source: "docs", using: NIOLeafFiles.defaultDocs(with: fileio))
     }
 
+    /// This registers the source "docs" in the given application
+    /// - Parameter app: The application in which to register the source.
     @inlinable
     public func addDefaultDocsSource(for app: Application) throws {
         try register(source: "docs", using: app.defaultDocsLeafSource)
@@ -62,11 +70,8 @@ extension LeafRenderer {
 
 extension Application.Leaf {
     /// Registers the tags needed to display the default docs.
-    /// Currently this is:
-    /// - escaped: `EscapeTag`
-    public func registerDocumentationTags() {
-        tags["escaped"] = EscapeTag()
-    }
+    /// Currently this does nothing since we don't have any specific tags. This is reserved as a future improvement
+    public func registerDocumentationTags() {}
 
     @inlinable
     public func setupRouteDocs() throws {
